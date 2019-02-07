659891f68e5136896ee36b8e538e41200aef3bdc
TiremanYT <33357049+TiremanYT@users.noreply.github.com>
2019-02-07 22:41:33 +0300

    Adden AltManager

---
+++ me/tireman/hexa/alts/AltManager.java
@@ -0,0 +1,24 @@
+/* —«•´¨*•.¸¸.* ..::** Pyro client **::… *.¸¸.•*¨`•»— */
+	/* http://xordevs.xyz | XorClients © 2017 */
+package me.tireman.hexa.alts;
+
+import java.util.ArrayList;
+
+
+public class AltManager {
+    public static Alt lastAlt;
+    public static ArrayList<Alt> registry;
+
+    static {
+        registry = new ArrayList();
+    }
+
+    public ArrayList<Alt> getRegistry() {
+        return registry;
+    }
+
+    public void setLastAlt(Alt alt2) {
+        lastAlt = alt2;
+    }
+}
+
